36ecbfd8d39baad73cb0168acb0889177a3d9501
Salih Mohammed <Salih-Mohammed@gmail.com>
2024-10-22 11:51:42 +0300

    add model Availability and route Availability

---
+++ app/dashboard/availability/page.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React from "react";
+
+function AvailabilityRoute() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Availability</CardTitle>
+          <CardDescription>
+            In this section you can manage your availability!
+          </CardDescription>
+        </CardHeader>
+        <form >
+            <CardContent> 
+                
+            </CardContent>
+        </form>
+      </Card>
+    </>
+  );
+}
+
+export default AvailabilityRoute;
